e26da6ff4d615f8f3f6a8e31107849ee4cac1f62
pict <121254233+pict888@users.noreply.github.com>
2026-05-18 10:14:13 +0530

    Create S-des.py

---
+++ S-des.py
@@ -0,0 +1,104 @@
+# Simplified DES (S-DES)
+IP  = [1,5,2,0,3,7,4,6]
+IP1 = [3,0,2,4,6,1,7,5]
+
+EP  = [3,0,1,2,1,2,3,0]
+
+P4  = [1,3,2,0]
+
+S0 = [
+    [1,0,3,2],
+    [3,2,1,0],
+    [0,2,1,3],
+    [3,1,3,2]
+]
+
+S1 = [
+    [0,1,2,3],
+    [2,0,1,3],
+    [3,0,1,0],
+    [2,1,0,3]
+]
+
+# Permutation
+def permute(bits, table):
+    return [bits[i] for i in table]
+
+# XOR
+def XOR(a, b):
+    return [ a[i] ^ b[i] for i in range(len(a))]
+
+# S-Box
+def sbox(bits, box):
+
+    row = (bits[0] << 1) + bits[3]
+    col = (bits[1] << 1) + bits[2]
+    val = box[row][col]
+    return [ (val >> 1) & 1, val & 1]
+
+# fk Function
+def fk(bits, key):
+    L = bits[:4]
+    R = bits[4:]
+    ep = permute(R, EP)
+    x = XOR(ep, key)
+    left  = x[:4]
+    right = x[4:]
+    s0 = sbox(left, S0)
+    s1 = sbox(right, S1)
+    p4 = permute(s0 + s1, P4)
+    newL = XOR(L, p4)
+    return newL + R
+
+# Swap
+def swap(bits):
+    return bits[4:] + bits[:4]
+
+# Encrypt
+def encrypt(text, K1, K2):
+    print("\nAfter IP:")
+    text = permute(text, IP)
+    print(text)
+    print("\nRound 1")
+    text = fk(text, K1)
+    print(text)
+    text = swap(text)
+    print("\nAfter Swap")
+    print(text)
+    print("\nRound 2")
+    text = fk(text, K2)
+    print(text)
+    text = permute(text, IP1)
+    return text
+
+# Decrypt
+def decrypt(text, K1, K2):
+    text = permute(text, IP)
+    text = fk(text, K2)
+    text = swap(text)
+    text = fk(text, K1)
+    text = permute(text, IP1)
+    return text
+
+# Convert string to bits
+def strToBits(s):
+    return [int(x) for x in s]
+
+# MAIN
+plainStr =
+input("Enter 8-bit Plaintext: ")
+k1Str = input("Enter 8-bit Key K1: ")
+k2Str = input("Enter 8-bit Key K2: ")
+plain = strToBits(plainStr)
+K1 = strToBits(k1Str)
+K2 = strToBits(k2Str)
+
+# Encryption
+cipher = encrypt(plain, K1, K2)
+print("\nEncrypted Text:")
+print(cipher)
+
+# Decryption
+text = decrypt(cipher, K1, K2)
+print("\nDecrypted Text:")
+print(text)
